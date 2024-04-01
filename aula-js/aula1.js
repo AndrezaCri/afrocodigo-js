c58ac13e795c9806bfb9df07c6257d9fe4b0623e
Andreza Cristina <andrezadesenvolvedoraweb@gmail.com>
2024-04-01 15:23:55 -0300

    first commit
    codigo aula1

---
+++ aula-js/aula1.js
@@ -0,0 +1,15 @@
+const cor = ("Azul")
+const fruta = ("maça")
+const estado = ("São Paulo")
+
+console.log(cor)
+console.log(fruta)
+console.log(estado)
+
+const nomePais = ("Brasil")
+const pontoTuristico = ("Rio de Janeiro")
+const capitalDoPais = ("Brasilia")
+const quantidadeDeEstado = 26
+const eDaAmericaLatina = true
+
+console.log(nomePais,pontoTuristico,capitalDoPais,quantidadeDeEstado,eDaAmericaLatina)
